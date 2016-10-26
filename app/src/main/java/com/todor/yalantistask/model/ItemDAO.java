f872c0296897f774885fa40aa398398cc0ca108c
iamtodor <todor.ilya@gmail.com>
2016-10-26 15:55:46 +0300

    Refactored and changed build version gradle

--- app/src/main/java/com/todor/yalantistask/model/ItemDAO.java
+++ app/src/main/java/com/todor/yalantistask/model/ItemDAO.java
@@ -9,7 +9,6 @@ public class ItemDAO {
 
     private static List<Item> filteredItems = new ArrayList<>();
     private static Realm realm = Realm.getDefaultInstance();
-    private static List<Item> returnedItems;
 
     public static List<Item> getItemsForDone() {
         filteredItems.clear();
